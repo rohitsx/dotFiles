b0a73b7ab5e05d4735187e00ca43b028d7a9f548
rohit <rohiitrb@gmail.com>
2025-04-21 09:57:32 +0530

    added treesitter

--- lua/config/option.lua
+++ lua/config/option.lua
@@ -1,12 +1,13 @@
+-- General Options
 local opt = vim.opt
 
 vim.cmd("colorscheme retrobox")
 
-opt.clipboard = "unnamedplus"
-opt.number = true
-opt.mouse = "a"
-opt.ignorecase = true
-opt.wrap = true
+opt.clipboard = "unnamedplus" -- Use system clipboard
+opt.number = true -- Show line numbers
+opt.mouse = "a" -- Enable mouse support
+opt.ignorecase = true -- Ignore case when searching
+opt.wrap = true -- Enable line wrapping
 
-opt.tabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 2 -- Number of spaces a tab counts for
+opt.shiftwidth = 2 -- Number of spaces for indentation
---
+++ lua/lsp/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "typescript", "javascript", "svelte", "python", "html", "css", "json" },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
+}
